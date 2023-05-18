d6ebcd3d8eecb6b32ea83a3917dd8ecd096cc67b
Daniel Grandoli <danig2994@gmail.com>
2023-05-18 19:58:19 -0300

    actualice

--- Vue/Vue01/src/App.vue
+++ Vue/Vue01/src/App.vue
@@ -2,6 +2,7 @@
   import { ref} from 'vue';
 
   var count = ref(0);
+  var msg = ref("Hola Mariano!!!");
 
   function increment(){
     count.value++;
@@ -10,26 +11,43 @@
   function decrement(){
     count.value--;
   }
+
   function cuadrado(){
-    count.value=count.value*count.value;
+    count.value = count.value * count.value;
   }
-  
+
   function cubo(){
-    count.value=count.value*count.value*count.value;
+    count.value = count.value * count.value * count.value;
+  }
+
+  function sqrt2(){
+    count.value = Math.pow(count.value,(1/2));
+  }
+
+  function sqrt3(){
+    count.value = Math.pow(count.value,(1/3));
   }
 </script>
 
 <template>
   <h1>¡Hola Mundo!</h1>
+
+  <div>
+    <input type="text" name="texto" :value="msg"
+        @input="event => msg = event.target.value">
+    <h2><span>Message: {{ msg }}</span></h2>
+  </div>
+
   <div>
     <h3>Count is: {{ count }}</h3>
   </div>
   <div>
     <button @click="increment">(+)</button> 
     <button @click="decrement">(-)</button>
-    <button @click="cuadrado">(^2)</button> 
+    <button @click="cuadrado">(^2)</button>
     <button @click="cubo">(^3)</button>
-
+    <button @click="sqrt2">(^-2)</button>
+    <button @click="sqrt3">(^-3)</button>
   </div>
 </template>
 
